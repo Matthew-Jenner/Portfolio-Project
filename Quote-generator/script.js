f193ebde40776677b04d5cf64f33f82799e032ba
Matthew Jenner <matthewjenner@hotmail.com>
2023-09-25 21:21:07 +0100

    button functionality linked for twitter share quote and new quote generator

--- Quote-generator/script.js
+++ Quote-generator/script.js
@@ -1,10 +1,27 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
 let apiQuotes = []
 
 // show new quote
 function newQuote(){
     // pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random()* apiQuotes.length)]
-    console.log(quote);
+//    check if author field is blank and replace it with 'unknown'
+if(!quote.author) {
+    authorText.textContent = 'Unknown'
+} else {
+    authorText.textContent = quote.author
+}
+// check quote length to determine styling
+if (quote.text.length > 75) {
+    quoteText.classList.add('long-quote')
+} else {
+    quoteText.classList.remove('long-quote')
+}
+    quoteText.textContent = quote.text;
 }
 
 // get quotes from api
@@ -21,5 +38,13 @@ async function getQuotes() {
 
 }
 
+// tweet quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
+    window.open(twitterUrl, '_blank')
+}
+// event listeners
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote)
 // on load
 getQuotes()
